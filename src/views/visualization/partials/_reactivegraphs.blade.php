b8b118d3c91223c32f3cf0bbd8d6ce2164ad239c
David Wambugu <davidwambugu@gmail.com>
2014-10-06 19:16:28 +0300

    upgrade

--- src/views/visualization/partials/_reactivegraphs.blade.php
+++ src/views/visualization/partials/_reactivegraphs.blade.php
@@ -1,17 +1,12 @@
  <div class="row padding-30">
         <div class="col-md-12">
-              <div class="panel panel-default col-md-6">
+              <div class="panel panel-default col-md-12">
                         <div class="panel-body">
                                     <div id="chart" style="width: 100%; height: 300px">
                                     </div>
                         </div>
               </div>
-               <div class="panel panel-default col-md-6">
-                        <div class="panel-body">
-                                    <div id="speedometer" style="width: 100%; height: 300px">
-                                    </div>
-                        </div>
-              </div>
+
               <div class="panel panel-default">
                         <div class="panel-body">
                                     <div id="gyro" style="width: 100%; height: 300px">
